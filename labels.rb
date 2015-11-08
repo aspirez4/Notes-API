633c1545ce50898c8d78bfc56cec90fa99a310e6
aspirez4 <nikii.ivanov@gmail.com>
2015-11-08 17:21:53 +0200

    Added Labels class

---
+++ labels.rb
@@ -0,0 +1,13 @@
+require 'active_record'
+
+class Labels < ActiveRecord::Base
+  def self.add(labels, note_id)
+    labels = labels.split(',')
+    labels.each do |label|
+      Labels.create(
+          note_id: note_id,
+          label: label.strip
+      )
+    end
+  end
+end
